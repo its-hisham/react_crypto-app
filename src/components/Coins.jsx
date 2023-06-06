e2b15922a32f271e7d3ab186b291b97978835ca4
Hisham <97hisham@gmail.com>
2023-06-06 14:26:22 +0530

    added loader page

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect,useState } from 'react'
 import axios from 'axios'
 import { server } from '../index'
-import { Container, HStack, Heading, VStack,Text,Image, Button} from '@chakra-ui/react';
+import { Container, HStack, Heading, VStack,Text,Image, Button, RadioGroup, Radio} from '@chakra-ui/react';
 import Loader from './Loader';
 import ErrorComponent from './ErrorComponent';
 import CoinCard from './CoinCard';
@@ -14,7 +14,7 @@ const Coins = () => {
   const [page,setPage] =useState(1);
   const [currency,setCurrency] =useState("inr");
 
-  const currencySymbol= currency==="inr"? "₹" : currency==="eur"?"€":"$"
+  const currencySymbol= currency==="INR"? "₹" : currency==="EUR" ? "€" : "$"
 
   const changePage= (page)=> {
     setPage(page);
@@ -50,18 +50,28 @@ if (error) return <ErrorComponent message={"error while fetching.."}/>;
       {loading? <Loader/>:(
 
         <>
+        <RadioGroup value={currency} onChange={setCurrency} p={8}>
+          <HStack spacing={"4"}>
+            <Radio value={"INR"}>INR</Radio>
+            <Radio value={"USD"}>USD</Radio>
+            <Radio value={"EUR"}>EUR</Radio>
+            
+          </HStack>
+        </RadioGroup>
         
-        <HStack wrap={"wrap"}>
+        <HStack justifyContent={'space-evenly'} wrap={"wrap"}>
          {coins.map((i)=>
          <CoinCard name={i.name}  img={i.image} symbol={i.symbol}  key={i.id} id={i.id} price={i.current_price} currencySymbol={currencySymbol}/>
          
          )}
         </HStack>
 
+        
+
         <HStack w={"full"} overflow={"auto"} p={8}>
          {
           btns.map((items,index)=>(
-            <Button bgColor={'blackAlpha.900'} color={'white'} onClick={()=>changePage(index+1)}>
+            <Button key={index} bgColor={'blackAlpha.900'} color={'white'} onClick={()=>changePage(index+1)}>
             {index+1}
           </Button>
           ))
