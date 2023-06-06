230571b642be99fc1ef861ecb166e1087cc481d8
Hisham <97hisham@gmail.com>
2023-06-06 14:03:46 +0530

    added pagination

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect,useState } from 'react'
 import axios from 'axios'
 import { server } from '../index'
-import { Container, HStack, Heading, VStack,Text,Image} from '@chakra-ui/react';
+import { Container, HStack, Heading, VStack,Text,Image, Button} from '@chakra-ui/react';
 import Loader from './Loader';
 import ErrorComponent from './ErrorComponent';
 import CoinCard from './CoinCard';
@@ -15,6 +15,13 @@ const Coins = () => {
   const [currency,setCurrency] =useState("inr");
 
   const currencySymbol= currency==="inr"? "₹" : currency==="eur"?"€":"$"
+
+  const changePage= (page)=> {
+    setPage(page);
+    setloading(true)
+  }
+
+  const btns=new Array(132).fill(1)
 
 useEffect(() => {
   
@@ -49,6 +56,16 @@ if (error) return <ErrorComponent message={"error while fetching.."}/>;
          <CoinCard name={i.name}  img={i.image} symbol={i.symbol}  key={i.id} id={i.id} price={i.current_price} currencySymbol={currencySymbol}/>
          
          )}
+        </HStack>
+
+        <HStack w={"full"} overflow={"auto"} p={8}>
+         {
+          btns.map((items,index)=>(
+            <Button bgColor={'blackAlpha.900'} color={'white'} onClick={()=>changePage(index+1)}>
+            {index+1}
+          </Button>
+          ))
+         }
         </HStack>
         
         
