a3c8d6a4dba1b18d78e038d5f90338d4ad50338e
Hisham <97hisham@gmail.com>
2023-06-06 15:36:23 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Coins from "./components/Coins";
-import CoinDetails from "./components/CoinCard";
 import Exchanges from "./components/Exchanges";
-
+import CoinDetails from "./components/CoinDetails";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}               />
-        <Route path="/coins" element={<Coins/>}               />
-        <Route path="/coin/:id" element={<CoinDetails/>}               />
-        <Route path="/exchanges" element={<Exchanges/>}               />
-       
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/coin/:id" element={<CoinDetails />} />
       </Routes>
+
+      <Footer />
     </Router>
   );
-  }
+}
 
 export default App;
--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,94 +1,90 @@
-import React, { useEffect,useState } from 'react'
-import axios from 'axios'
-import { server } from '../index'
-import { Container, HStack, Heading, VStack,Text,Image, Button, RadioGroup, Radio} from '@chakra-ui/react';
-import Loader from './Loader';
-import ErrorComponent from './ErrorComponent';
-import CoinCard from './CoinCard';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { server } from "../index";
+import { Button, Container, HStack, Radio, RadioGroup } from "@chakra-ui/react";
+import Loader from "./Loader";
+import ErrorComponent from "./ErrorComponent";
+import CoinCard from "./CoinCard";
 
 const Coins = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("inr");
 
-  const [coins,setCoins] =useState([]);
-  const [loading,setloading] =useState(true);
-  const [error,setError] =useState(false);
-  const [page,setPage] =useState(1);
-  const [currency,setCurrency] =useState("inr");
+  const currencySymbol =
+    currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
 
-  const currencySymbol= currency==="INR"? "₹" : currency==="EUR" ? "€" : "$"
-
-  const changePage= (page)=> {
+  const changePage = (page) => {
     setPage(page);
-    setloading(true)
-  }
+    setLoading(true);
+  };
 
-  const btns=new Array(132).fill(1)
+  const btns = new Array(132).fill(1);
 
-useEffect(() => {
-  
-const fetchCoins = async ()=> {
-  try {
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const { data } = await axios.get(
+          `${server}/coins/markets?vs_currency=${currency}&page=${page}`
+        );
+        setCoins(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+  }, [currency, page]);
 
-    const {data} =await axios.get(`${server}/coins/markets?vs_currency=${currency}&page=${page}`)
-
-  setCoins(data);
-  
-  setloading(false);
-    
-  } catch (error) {
-    setError(true);
-    setloading(false);
-  }
-};
-fetchCoins(currency,page);  
-
-}, [])
-
-if (error) return <ErrorComponent message={"error while fetching.."}/>;
+  if (error) return <ErrorComponent message={"Error While Fetching Coins"} />;
 
   return (
-    <Container maxW={"container.xl"} >
-      {loading? <Loader/>:(
-
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
         <>
-        <RadioGroup value={currency} onChange={setCurrency} p={8}>
-          <HStack spacing={"4"}>
-            <Radio value={"INR"}>INR</Radio>
-            <Radio value={"USD"}>USD</Radio>
-            <Radio value={"EUR"}>EUR</Radio>
-            
+          <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+            <HStack spacing={"4"}>
+              <Radio value={"inr"}>INR</Radio>
+              <Radio value={"usd"}>USD</Radio>
+              <Radio value={"eur"}>EUR</Radio>
+            </HStack>
+          </RadioGroup>
+
+          <HStack wrap={"wrap"} justifyContent={"space-evenly"}>
+            {coins.map((i) => (
+              <CoinCard
+                id={i.id}
+                key={i.id}
+                name={i.name}
+                price={i.current_price}
+                img={i.image}
+                symbol={i.symbol}
+                currencySymbol={currencySymbol}
+              />
+            ))}
           </HStack>
-        </RadioGroup>
-        
-        <HStack justifyContent={'space-evenly'} wrap={"wrap"}>
-         {coins.map((i)=>
-         <CoinCard name={i.name}  img={i.image} symbol={i.symbol}  key={i.id} id={i.id} price={i.current_price} currencySymbol={currencySymbol}/>
-         
-         )}
-        </HStack>
 
-        
-
-        <HStack w={"full"} overflow={"auto"} p={8}>
-         {
-          btns.map((items,index)=>(
-            <Button key={index} bgColor={'blackAlpha.900'} color={'white'} onClick={()=>changePage(index+1)}>
-            {index+1}
-          </Button>
-          ))
-         }
-        </HStack>
-        
-        
+          <HStack w={"full"} overflowX={"auto"} p={"8"}>
+            {btns.map((item, index) => (
+              <Button
+                key={index}
+                bgColor={"blackAlpha.900"}
+                color={"white"}
+                onClick={() => changePage(index + 1)}
+              >
+                {index + 1}
+              </Button>
+            ))}
+          </HStack>
         </>
       )}
-
     </Container>
-  )
-}
+  );
+};
 
-
-
-
-
-
-export default Coins
+export default Coins;
