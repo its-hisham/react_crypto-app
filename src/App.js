453510051b44584370b3e786f9d9213f8a0d4097
Hisham <97hisham@gmail.com>
2023-06-06 13:37:51 +0530

    added coin card page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Coins from "./components/Coins";
-import CoinDetails from "./components/CoinDetails";
+import CoinDetails from "./components/CoinCard";
 import Exchanges from "./components/Exchanges";
 
 
--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -4,6 +4,7 @@ import { server } from '../index'
 import { Container, HStack, Heading, VStack,Text,Image} from '@chakra-ui/react';
 import Loader from './Loader';
 import ErrorComponent from './ErrorComponent';
+import CoinCard from './CoinCard';
 
 const Coins = () => {
 
@@ -12,6 +13,8 @@ const Coins = () => {
   const [error,setError] =useState(false);
   const [page,setPage] =useState(1);
   const [currency,setCurrency] =useState("inr");
+
+  const currencySymbol= currency==="inr"? "₹" : currency==="eur"?"€":"$"
 
 useEffect(() => {
   
@@ -43,7 +46,7 @@ if (error) return <ErrorComponent message={"error while fetching.."}/>;
         
         <HStack wrap={"wrap"}>
          {coins.map((i)=>
-         <ExchangeCard name={i.name}  img={i.image} rank={i.trust_score_rank} url={i.url} key={i.id}/>
+         <CoinCard name={i.name}  img={i.image} symbol={i.symbol}  key={i.id} id={i.id} price={i.current_price} currencySymbol={currencySymbol}/>
          
          )}
         </HStack>
@@ -58,25 +61,7 @@ if (error) return <ErrorComponent message={"error while fetching.."}/>;
 
 
 
-const ExchangeCard = ({name,img,rank,url})=>(
-  
-  <a href={url} target={'blank'}>
 
-    <VStack w={'52'} shadow={"lg"} p="8" borderRadius={'lg'} transition={'all 0.3s'} m={'4'}   css={{
-      '&:hover':{
-        transform:'scale(1.1)'
-      }
-    }}>
-      <Image src={img} w={'10'} h={'10'} objectFit={'contain'} alt={'Exchange'}/>
-      <Heading size={'md'} noOfLines={'1'}>{rank}</Heading>
-        
-        <Text noOfLines={'1'}>{name}</Text>
-
-    </VStack>
-
-  </a>
-
-)
 
 
 export default Coins
